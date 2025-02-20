0ddb1850b4b3f622c6cda99e9e2eb20d1b2ff559
herodhujun <13552232411@163.com>
2025-02-20 20:07:19 +0800

    go init

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
 const (
-	PORT       = ":8080"
-	APP_NAME   = "gostudy"
-	APP_SECRET = "6YJSuc50uJ18zj45"
-	API_EXPIRY = "120"
+	PORT          = ":8080"
+	APP_NAME      = "gostudy"
+	APP_SECRET    = "6YJSuc50uJ18zj45"
+	API_EXPIRY    = "120"
+	Log_FILE_PATH = "E:/project/go/gostudy"
+	LOG_FILE_NAME = "system.log"
 )
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/learn/gostudy/config"
+	"github.com/learn/gostudy/middleware"
 	"github.com/learn/gostudy/router"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode) // 默认为 debug 模式，设置为发布模式
 	engine := gin.Default()
-	router.InitRouter(engine) // 设置路由
+	engine.Use(middleware.LoggerToFile()) //设置日志记录拦截
+	router.InitRouter(engine)             // 设置路由
+
 	engine.Run(config.PORT)
+
 }
